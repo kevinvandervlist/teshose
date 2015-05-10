13673428a596fb68da80d9845ebeaf3c5beea35c
Kevin van der Vlist <kvdvlist@sogyo.nl>
2015-05-10 22:23:07 +0200

    Scaffolding

--- teshose-core/src/main/java/nl/soqua/teshose/Teshose.java
+++ teshose-core/src/main/java/nl/soqua/teshose/Teshose.java
@@ -4,26 +4,33 @@ import lombok.extern.slf4j.Slf4j;
 import nl.soqua.teshose.config.Configuration;
 import nl.soqua.teshose.config.impl.ConcreteConfiguration;
 import nl.soqua.teshose.telegram.TelegramAPIState;
+import org.telegram.api.auth.TLCheckedPhone;
 import org.telegram.api.engine.ApiCallback;
 import org.telegram.api.engine.AppInfo;
 import org.telegram.api.engine.TelegramApi;
+import org.telegram.api.requests.TLRequestAuthCheckPhone;
 import org.telegram.mtproto.pq.Authorizer;
 import org.telegram.mtproto.pq.PqAuth;
+
+import java.io.IOException;
 
 @Slf4j
 public class Teshose {
 
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     System.out.println("Hello world!");
 
     Configuration config = ConcreteConfiguration.getInstance();
 
-    TelegramAPIState state = new TelegramAPIState();
+    TelegramAPIState state = new TelegramAPIState(config);
     AppInfo info = config.getAppInfo();
 
+    log.info("Creating new authorizer.");
     Authorizer auth = new Authorizer();
-    PqAuth pqAuth = auth.doAuth(config.getAddress(), config.getPort());
+    log.info("auth.doAuth()");
+    //PqAuth pqAuth = auth.doAuth(config.getAddress(), config.getPort());
 
+    log.info("Creating telegram API:");
     TelegramApi api = new TelegramApi(state, info, new ApiCallback()
     {
       @Override
@@ -37,5 +44,17 @@ public class Teshose {
         System.out.println("onUpdatesInvalidated: " + api);
       }
     });
+
+    String phoneNumber = "";
+    TLRequestAuthCheckPhone checkPhone = new TLRequestAuthCheckPhone(phoneNumber);
+
+    // Call service synchronously
+    log.info("do checkPhone sync.");
+    TLCheckedPhone checkedPhone = api.doRpcCall(checkPhone);
+    boolean invited = checkedPhone.getPhoneInvited();
+    log.info("Invited: " + invited);
+    boolean registered = checkedPhone.getPhoneRegistered();
+    log.info("Registered: " + registered);
+    log.info("Exitting...");
   }
 }
--- teshose-core/src/main/java/nl/soqua/teshose/telegram/TelegramAPIState.java
+++ teshose-core/src/main/java/nl/soqua/teshose/telegram/TelegramAPIState.java
@@ -1,60 +1,76 @@
 package nl.soqua.teshose.telegram;
 
 import lombok.extern.slf4j.Slf4j;
+import nl.soqua.teshose.config.Configuration;
 import org.telegram.api.TLConfig;
 import org.telegram.api.engine.storage.AbsApiState;
 import org.telegram.mtproto.state.AbsMTProtoState;
 import org.telegram.mtproto.state.ConnectionInfo;
 
+import java.io.Serializable;
+
 @Slf4j
-public class TelegramAPIState implements AbsApiState {
+public class TelegramAPIState implements AbsApiState,Serializable {
+
+  private final Configuration configuration;
+
+  public TelegramAPIState(Configuration configuration) {
+    this.configuration = configuration;
+    // TODO: Figure out how to handle persistence.
+  }
+
   @Override
   public int getPrimaryDc() {
+    log.info("getPrimaryDc() called.");
     return 0;
   }
 
   @Override
   public boolean isAuthenticated(final int dcId) {
+    log.info("isAuthenticated(" + dcId + ") called.");
     return false;
   }
 
   @Override
   public void setAuthenticated(final int dcId, final boolean auth) {
-
+    log.info("setAuthenticated(" + dcId + ", " + auth + ") called.");
   }
 
   @Override
   public void updateSettings(final TLConfig config) {
-
+    log.info("updateSettings(" + config + ") called.");
   }
 
   @Override
   public byte[] getAuthKey(final int dcId) {
+    log.info("getAuthKey(" + dcId + ") called.");
     return new byte[0];
   }
 
   @Override
   public void putAuthKey(final int dcId, final byte[] key) {
-
+    log.info("putAuthKey(" + dcId + ", " + key + ") called.");
   }
 
   @Override
   public ConnectionInfo getConnectionInfo(final int dcId) {
+    log.info("getConnectionInfo(" + dcId + ") called.");
     return null;
   }
 
   @Override
   public AbsMTProtoState getMtProtoState(final int dcId) {
+    log.info("getMtProtoState(" + dcId + ") called.");
     return null;
   }
 
   @Override
   public void resetAuth() {
-
+    log.info("resetAuth() called.");
   }
 
   @Override
   public void reset() {
-
+    log.info("reset() called.");
   }
 }
